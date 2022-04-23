b8fc9a03d401be63e2fdb335d8b2829e7a211fa3
Willian Per <wsresilienttasteshopw@gmail.com>
2022-04-23 15:22:19 -0400

    Abordando implementaçãos de requisição AJAX com  a DOM para exposição dos dados em uma tabela

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express')
 
 const app = express()
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
